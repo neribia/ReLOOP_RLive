96409a43dacbcdd3bb45fe277ba8f1423c5b255b
neribia <149081906+neribia@users.noreply.github.com>
2025-11-20 09:46:58 +0100

    Remove file and move to other files

--- tests/rlive-common/test_encoding.py
+++ tests/rlive-common/test_encoding.py
@@ -1,20 +1,22 @@
 import unittest
-from rlive_common.core.pydantic_types import _encode_image, _decode_image
+from rlive_common.core.pydantic_types import (
+    _encode_image,
+    _decode_image,
+    _ndarray_serializer,
+    _ndarray_before_validator,
+)
 import numpy as np
 
 
 class TestImageEncoding(unittest.TestCase):
-
     def test_image_encoding_decoding(self):
         original_array = np.random.randint(0, 255, size=(2, 4, 3), dtype=np.uint8)
         encoded = _encode_image(original_array)
         decoded_array = _decode_image(encoded)
         self.assertTrue(np.array_equal(original_array, decoded_array))
-    
-    def test_invalid_input_for_decoding(self):
 
+    def test_numpy_encoding_decoding(self):
         original_array = np.random.randint(0, 255, size=(2, 4, 3), dtype=np.uint8)
-
-        with self.assertRaises(TypeError):
-            _decode_image(original_array)
-
+        encoded = _ndarray_serializer(original_array)
+        decoded_array = _ndarray_before_validator(encoded)
+        self.assertTrue(np.array_equal(original_array, decoded_array))
