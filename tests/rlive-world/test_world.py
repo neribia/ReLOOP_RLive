bff670bdce435ddeaea3fe6a86474f8404677a0f
neribia <149081906+neribia@users.noreply.github.com>
2025-11-21 10:08:42 +0100

    Remove async from tests

--- tests/rlive-world/test_world.py
+++ tests/rlive-world/test_world.py
@@ -8,32 +8,30 @@ from rlive_world.world import World
 
 
 class ToyWorld(World):
-    async def _make_observation(self) -> np.ndarray:
-        await asyncio.sleep(0)  # yield control; stands in for real async IO
+     def _make_observation(self) -> np.ndarray:
         return np.zeros((100, 100), dtype=np.uint32)  # (480, 640))
 
 
 class TestWorld(unittest.TestCase):
     def test_make_observation(self):
         world = World()
-        obs = asyncio.run(world._make_observation())
+        obs = world._make_observation()
 
         self.assertIsInstance(obs, np.ndarray)
         self.assertTrue(obs.dtype == np.float32)
-        print(f"n-Dimension: {obs.shape}, {obs.ndim}")
         self.assertTrue(obs.ndim in (1, 2, 3)) # FIXME: remove 1 when camera is implemented in this function
 
     def test_reset(self):
         world = ToyWorld()
         request = ResetRequest()
-        result = asyncio.run(world.reset(request))
+        result = world.reset(request)
 
         self.assertIsInstance(result, Response)
 
     def test_step(self):
         world = ToyWorld()
         request = StepRequest(action=5)
-        result, img = asyncio.run(world.step(request))
+        result, img = world.step(request)
 
         self.assertIsInstance(result, Response)
         self.assertIsInstance(img, np.ndarray)
