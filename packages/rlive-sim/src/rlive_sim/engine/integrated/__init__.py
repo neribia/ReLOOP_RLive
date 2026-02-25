6d6bde0509eb8bed4bd47363fb4849725626fc62
neribia <149081906+neribia@users.noreply.github.com>
2026-02-25 18:25:18 +0100

    First shot with Copilot

--- packages/rlive-sim/src/rlive_sim/engine/integrated/__init__.py
+++ packages/rlive-sim/src/rlive_sim/engine/integrated/__init__.py
@@ -1,7 +1,9 @@
 """Integrated engine implementations."""
 
 from rlive_sim.engine.integrated.godot_integrated_engine import GodotIntegratedEngine
+from rlive_sim.engine.integrated.mujoco_integrated_engine import MujocoIntegratedEngine
 
 __all__ = [
     "GodotIntegratedEngine",
+    "MujocoIntegratedEngine",
 ]
